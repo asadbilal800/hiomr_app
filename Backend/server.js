e466320236864620b49b8b7e3aaa5e0f831f22ee
asad_dev <asadbilal800@gmail.com>
2024-02-22 00:34:41 +0500

    added express in the server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,21 @@
-//backend code here
+// Importing required modules
+const express = require('express');
+
+// Creating an instance of Express
+const app = express();
+const port = 3000; // on default it is this.
+
+// Define the /checkEmail endpoint
+app.get('/checkEmail', (req, res) => {
+ //will call sql db to check if it exists.
+});
+
+function isValidEmailFormat(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
