f7a3143b55fe10de41339e1ae4200ae45cbb3a80
asad_dev <asadbilal800@gmail.com>
2024-02-23 00:02:19 +0500

    added environmental variable changes

---
+++ src/environments/environment.ts
@@ -0,0 +1,22 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  wepay_production: false,
+  api_url: 'http://localhost:4200/',
+  driveFolderId: "0AFtJBV_Z9gitUk9PVA",
+  dbUser : 'asad',
+  dbuUserPwd : 'TbqFxKQd4!z%nrj',
+  dbConnectionURL : 'jdbc:google:mysql://mainframe-312100:us-west3:database/production_image_omr'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
