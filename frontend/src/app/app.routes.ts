1dbbfd60c73838c696b240df6ca76ddcf32929d4
asad_dev <asadbilal800@gmail.com>
2024-03-23 01:45:31 +0500

    updated route configuration

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { MainPageComponent } from './components/main/main.component';
 import { AuthGuard } from './services/auth-guard.service';
 
 export const routes: Routes = [
-  { path: 'home', component:MainPageComponent },
-  { path: 'email-page', loadComponent: () => import('./components/email/email.component').then(mod => mod.EmailPageComponent),canActivate:[AuthGuard] },
+  { path: 'home', component:MainPageComponent,
+  children:[
+  { path: 'email-page', loadComponent: () => import('./components/email/email.component').then(mod => mod.EmailPageComponent) },
   { path: 'submitting-doctor', loadComponent: () => import('./components/submitting-doctor/submitting-doctor.component').then(mod => mod.SubmittingDoctorComponent),canActivate:[AuthGuard] },
-  { path: 'match-practice', loadComponent: () => import('./components/match-practice/match-practice.component').then(mod => mod.MatchPracticeComponent),canActivate:[AuthGuard] },
-  { path: '',   redirectTo: '/home', pathMatch: 'full' }, // Redirect empty path to email-page
-  { path: '**', redirectTo: '/home' } // Wildcard route for any other unmatched routes
+  { path: 'match-practice', loadComponent: () => import('./components/match-practice/match-practice.component').then(mod => mod.MatchPracticeComponent)},
+  { path: '',   redirectTo: 'email-page', pathMatch: 'full'}, // Redirect empty path to email-page
+]
+},
+  { path: '',   redirectTo: 'home', pathMatch: 'full' }, // Redirect empty path to email-page
+  { path: '**', redirectTo: 'home' } // Wildcard route for any other unmatched routes
 ];
--- frontend/src/app/components/main/main.component.ts
+++ frontend/src/app/components/main/main.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { EmailPageComponent } from '../email/email.component';
 import { FooterComponent } from "../footer/footer.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-main',
     standalone: true,
     templateUrl: './main.component.html',
     styleUrl: './main.component.css',
-    imports: [EmailPageComponent, FooterComponent]
+    imports: [EmailPageComponent, FooterComponent,RouterOutlet]
 })
 export class MainPageComponent {
 
