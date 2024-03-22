d29a9233307181bbce1c61c007de27290c420142
asad_dev <asadbilal800@gmail.com>
2024-03-23 01:52:42 +0500

    email url changes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,10 +5,10 @@ import { AuthGuard } from './services/auth-guard.service';
 export const routes: Routes = [
   { path: 'home', component:MainPageComponent,
   children:[
-  { path: 'email-page', loadComponent: () => import('./components/email/email.component').then(mod => mod.EmailPageComponent) },
+  { path: 'email', loadComponent: () => import('./components/email/email.component').then(mod => mod.EmailPageComponent) },
   { path: 'submitting-doctor', loadComponent: () => import('./components/submitting-doctor/submitting-doctor.component').then(mod => mod.SubmittingDoctorComponent),canActivate:[AuthGuard] },
   { path: 'match-practice', loadComponent: () => import('./components/match-practice/match-practice.component').then(mod => mod.MatchPracticeComponent)},
-  { path: '',   redirectTo: 'email-page', pathMatch: 'full'}, // Redirect empty path to email-page
+  { path: '',   redirectTo: 'email', pathMatch: 'full'}, // Redirect empty path to email-page
 ]
 },
   { path: '',   redirectTo: 'home', pathMatch: 'full' }, // Redirect empty path to email-page
