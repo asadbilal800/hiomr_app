872c15d3e61c137d5a399c1a4a3f3781fc7d79ea
asad_dev <asadbilal800@gmail.com>
2024-04-06 03:32:39 +0500

    frontend fixes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,6 +8,7 @@ export const routes: Routes = [
   { path: 'email', loadComponent: () => import('./components/email/email.component').then(mod => mod.EmailPageComponent) },
   { path: 'submitting-doctor', loadComponent: () => import('./components/submitting-doctor/submitting-doctor.component').then(mod => mod.SubmittingDoctorComponent),canActivate:[AuthGuard] },
   { path: 'match-practice', loadComponent: () => import('./components/match-practice/match-practice.component').then(mod => mod.MatchPracticeComponent)},
+  { path: 'registration', loadComponent: () => import('./components/registration/registration.component').then(mod => mod.RegistrationComponent)},
   { path: '',   redirectTo: 'email', pathMatch: 'full'}, // Redirect empty path to email-page
 ]
 },
