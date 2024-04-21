9b19ee29b4851ada9524cd29759ecdd210bfeecb
asad_dev <asadbilal800@gmail.com>
2024-04-21 15:16:30 +0500

    dynamically iterated doctor list in radio list for submitting doctor

--- frontend/src/app/components/submitting-doctor/submitting-doctor.component.ts
+++ frontend/src/app/components/submitting-doctor/submitting-doctor.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../../services/shared.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-submitting-doctor',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './submitting-doctor.component.html',
   styleUrl: './submitting-doctor.component.css'
 })
 export class SubmittingDoctorComponent implements OnInit {
 
   userData: any ={};
+  allUserData: any = {};
   emailRelatedData: any = {};
   constructor(private sharedService: SharedService){
      console.log(this.sharedService.emailRelatedData);
@@ -19,6 +21,7 @@ export class SubmittingDoctorComponent implements OnInit {
 
   ngOnInit(): void {
     this.userData = this.sharedService.userData[0];
+    this.allUserData = this.sharedService.userData;
     this.emailRelatedData = this.sharedService.emailRelatedData;
     
   }
