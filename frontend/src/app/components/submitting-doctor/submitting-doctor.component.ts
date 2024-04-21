853710ef7c1d82dc299d044d9ea6fb81c67cfd95
asad_dev <asadbilal800@gmail.com>
2024-04-21 14:51:13 +0500

    integrated user details in submitting doctor

--- frontend/src/app/components/submitting-doctor/submitting-doctor.component.ts
+++ frontend/src/app/components/submitting-doctor/submitting-doctor.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../../services/shared.service';
 
 @Component({
   selector: 'app-submitting-doctor',
@@ -7,6 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './submitting-doctor.component.html',
   styleUrl: './submitting-doctor.component.css'
 })
-export class SubmittingDoctorComponent {
+export class SubmittingDoctorComponent implements OnInit {
+
+  userData: any ={};
+  emailRelatedData: any = {};
+  constructor(private sharedService: SharedService){
+     console.log(this.sharedService.emailRelatedData);
+     console.log(this.sharedService.userData);
+  }
+
+  ngOnInit(): void {
+    this.userData = this.sharedService.userData[0];
+    this.emailRelatedData = this.sharedService.emailRelatedData;
+    
+  }
 
 }
