8bb41c62cf9965f2a2e1b711957ad4f6d5de6679
asad_dev <asadbilal800@gmail.com>
2024-05-04 01:43:11 +0500

    patient form integration

--- frontend/src/app/components/patient-info/patient-info.component.ts
+++ frontend/src/app/components/patient-info/patient-info.component.ts
@@ -1,17 +1,35 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgxDropzoneModule } from 'ngx-dropzone';
 
 @Component({
   selector: 'app-patient-info',
   standalone: true,
-  imports: [NgxDropzoneModule,CommonModule],
+  imports: [NgxDropzoneModule,CommonModule,ReactiveFormsModule],
   templateUrl: './patient-info.component.html',
   styleUrl: './patient-info.component.css'
 })
-export class PatientInfoComponent {
-// in app.component.ts
-public files: File[] = [];
+export class PatientInfoComponent implements OnInit {
+  public files: File[] = [];
+  patientForm: FormGroup = {} as any;
+  sexType: string = '';
+  imageType: string = '';
+
+
+constructor(private formBuilder: FormBuilder){}
+
+ngOnInit(): void {
+  this.patientForm = this.formBuilder.group({
+    imageDate: [null, [Validators.required]], // date object required
+    internalId: ['', [Validators.required]], // string required
+    firstName: ['', [Validators.required]], // string required
+    lastName: ['', [Validators.required]], // string required
+    birthDate: [null, [Validators.required]], // date object required
+  }); 
+}
+
+
 
 onSelect(event) {
   console.log(event);
@@ -22,4 +40,16 @@ onRemove(event) {
   console.log(event);
   this.files.splice(this.files.indexOf(event), 1);
 }
+
+selectedOption(event,type:string){
+  if(type == 'imageType'){
+    this.imageType = event.target.value;
+  }
+  else this.sexType = event.target.value;
+
+  console.log(this.sexType);
+  console.log(this.imageType);
+  console.log(this.patientForm.value);
+}
+
 }
