ed1b5676ed907a655e8f6c214be4c400f42678ac
asad_dev <asadbilal800@gmail.com>
2024-05-04 01:12:37 +0500

    pushed dnd code

--- frontend/src/app/components/patient-info/patient-info.component.ts
+++ frontend/src/app/components/patient-info/patient-info.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgxDropzoneModule } from 'ngx-dropzone';
 
 @Component({
   selector: 'app-patient-info',
   standalone: true,
-  imports: [],
+  imports: [NgxDropzoneModule,CommonModule],
   templateUrl: './patient-info.component.html',
   styleUrl: './patient-info.component.css'
 })
 export class PatientInfoComponent {
+// in app.component.ts
+public files: File[] = [];
 
+onSelect(event) {
+  console.log(event);
+  this.files.push(...event.addedFiles);
+}
+
+onRemove(event) {
+  console.log(event);
+  this.files.splice(this.files.indexOf(event), 1);
+}
 }
