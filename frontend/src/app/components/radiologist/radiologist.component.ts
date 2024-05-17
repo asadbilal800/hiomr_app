f88e1c844b9b0c587fe5d479f4e27b4910595c7a
asad_dev <asadbilal800@gmail.com>
2024-05-17 16:02:37 +0500

    added ngif directive and button visibility to form submission

--- frontend/src/app/components/radiologist/radiologist.component.ts
+++ frontend/src/app/components/radiologist/radiologist.component.ts
@@ -1,17 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-radiologist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './radiologist.component.html',
   styleUrl: './radiologist.component.css'
 })
 export class RadiologistComponent {
 
   radioLogist: string;
-  rush: string;
-  stat: string;
+  rush: string = '0'
+  stat: string = '0'
   constructor()
   {}
 
@@ -22,6 +23,10 @@ export class RadiologistComponent {
     if(e.target.id == 'stat') {
       this.stat = (e.target.checked) ? '1' : '0'
     } 
+  }
+
+  submitPatientInfo(){
+    
   }
 
 }
