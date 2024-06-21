bf2f12b74ba75916a895e6a865e4fe93d4b88410
asad_dev <asadbilal800@gmail.com>
2024-06-22 03:37:50 +0500

    mouting,setup call,submission and messages added

--- frontend/src/app/components/stripe/stripe.component.ts
+++ frontend/src/app/components/stripe/stripe.component.ts
@@ -42,4 +42,52 @@ export class StripeComponent implements OnInit {
       
     }
   }
+
+   async submitIntentForm(event){  
+    event.preventDefault();
+    event.stopPropagation();
+    await this.finalizeStripe();
+  }
+
+  async  finalizeStripe(){
+    document.getElementById("StripeButton").innerHTML = 'Sending...';
+    document.getElementById("chooseInvoices").classList.add('d-none');
+    document.getElementById("invoiceMessage").classList.add('d-none');
+    document.getElementById('stripe-text').style.display = 'block'
+    document.getElementById('stripe-error').style.display = 'none';
+    const {error} = await this.sharedSerice.stripe.confirmSetup({
+      elements: this.stripeElement,
+      redirect:"if_required"
+    });
+    if (error) {this.showErrorMessageStripe();} 
+    else { this.showSuccessMessageStripe();}
+  }
+  
+   showErrorMessageStripe(){
+      document.getElementById('stripe-text').style.display = 'none'
+      document.getElementById('stripe-error').style.display = 'block'
+      document.getElementById("StripeButton").innerHTML = 'First Case Fee ($50)';
+      document.getElementById('stripe-success').style.display = 'none'
+      document.getElementById("stripe-body").style.cursor = "auto";
+  }
+  
+  showSuccessMessageStripe(){
+    let practiceId = localStorage.getItem('practiceId');
+    //set PAYMENT BIT HERE
+    localStorage.setItem('payment','1');
+      document.getElementById('stripe-text').style.display = 'none'
+      document.getElementById('stripe-error').style.display = 'none'
+      document.getElementById('stripe-success').style.display = 'block'
+      document.getElementById("payment-element").classList.add('d-none');
+      document.getElementById('discountDropdown').classList.add('d-none');
+      document.getElementById('stripeAuthorizeButton').classList.add('d-none');
+      document.getElementById('stripeLogoInside').classList.add('d-none');
+      var buttons = document.getElementsByClassName("stripe-link"); 
+      for(var i = 0; i < buttons.length; i++){
+          (buttons[i] as any).style.cursor = "pointer"};
+      document.getElementById("submitAnotherCaseButton").style.cursor = "pointer";
+      document.getElementById("stripe-body").style.cursor = "auto";
+      document.getElementById("payment-span").innerHTML = "Preauthorization Successful";
+      document.getElementById("autopayButton").classList.replace('btn-payment','btn-success');
+  }
 }
